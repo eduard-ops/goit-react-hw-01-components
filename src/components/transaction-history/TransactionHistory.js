d5c2a6c141d3399ffb126ce00dff3076a99a373b
eduard-ops <epustynnik@gmail.com>
2022-03-24 05:14:39 +0200

    TransactionHistory.js

---
+++ src/components/transaction-history/TransactionHistory.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+import s from '../transaction-history/Transaction.module.css';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <div className={s.wrapper}>
+      <table className="transaction-history">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ),
+};
